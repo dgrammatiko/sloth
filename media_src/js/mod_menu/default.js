0ead632ebbc1c3b837e8f8a1262dab60e991045f
dGrammatiko <d.grammatiko@gmail.com>
2020-05-11 00:51:56 +0200

    minor fixes

--- media_src/js/mod_menu/default.js
+++ media_src/js/mod_menu/default.js
@@ -1,12 +1,10 @@
-(() => {
-  const burgerElement = document.querySelector('.burger');
-  const overlayElement = document.querySelector('.overlay');
-  function onClick(ev) {
-    burgerElement.classList.toggle('clicked');
-    overlayElement.classList.toggle('show');
-    document.querySelector('.navbar').classList.toggle('show');
-    document.querySelector('body').classList.toggle('overflow');
-  }
-  document.querySelector('.burger').addEventListener('click', onClick)
-  document.querySelector('.overlay').addEventListener('click', onClick)
-  })();
+const burgerElement = document.querySelector('.burger');
+const overlayElement = document.querySelector('.overlay');
+function onClick(ev) {
+  burgerElement.classList.toggle('clicked');
+  overlayElement.classList.toggle('show');
+  document.querySelector('.navbar').classList.toggle('show');
+  document.querySelector('body').classList.toggle('overflow');
+}
+document.querySelector('.burger').addEventListener('click', onClick)
+document.querySelector('.overlay').addEventListener('click', onClick)
