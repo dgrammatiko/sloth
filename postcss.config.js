0450931f45553a6b6ee864af07be22b8ad13c406
dGrammatiko <d.grammatiko@gmail.com>
2020-05-29 11:28:43 +0200

    upd

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,15 @@
 module.exports = ctx => ({
-    plugins: {
-
-      'postcss-easy-import': {
-        extensions: '.pcss'
-      },
-      cssnano: {}
-    }
-  });
+  plugins: {
+    // 'postcss-import': {},
+    'postcss-easy-import': {
+      extensions: '.pcss'
+    },
+    'postcss-inline-svg': {},
+    'postcss-mixins': {},
+    'postcss-simple-vars': {},
+    'postcss-nested': {},
+    'postcss-combine-media-query': {},
+    'autoprefixer': {},
+    cssnano: {},
+  }
+});
