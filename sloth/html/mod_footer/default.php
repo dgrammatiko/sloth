47dc997c28b0e69f80f75f5cb9d75a77237d761c
dGrammatiko <d.grammatiko@gmail.com>
2020-07-16 19:25:09 +0200

    footer typo

--- sloth/html/mod_footer/default.php
+++ sloth/html/mod_footer/default.php
@@ -11,5 +11,5 @@ $wa->useStyle('mod_footer.default');
 echo
 '<footer class="main">',
   '<div class="items">Sloth is a free joomla4  PWA template</div>',
-  '<div class="items">Copyright Ⓒ' . date("Y") . '  <a href="httsp://dgrammatiko.online">D. Grammatikogiannis</a></div>',
+  '<div class="items">Copyright Ⓒ' . date("Y") . '  <a href="https://dgrammatiko.online">D. Grammatikogiannis</a></div>',
 '</footer>';
