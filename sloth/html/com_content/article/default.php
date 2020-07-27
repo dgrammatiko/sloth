490ae3e15336fe75aa5834df5876f0760024472a
dGrammatiko <d.grammatiko@gmail.com>
2020-07-27 15:44:42 +0200

    fix images

--- sloth/html/com_content/article/default.php
+++ sloth/html/com_content/article/default.php
@@ -5,7 +5,7 @@ $images = json_decode($this->item->images);
 $image = '';
 
 if (!empty($images->image_intro)) {
-  $image = '<img src="' . $images->image_intro . '" alt="' . $images->image_intro_alt .'" style="width: 100% !important;"/>';
+  $image = '<img src="' . $images->image_intro . '" alt="' . $images->image_intro_alt .'"/>';
   if (\Joomla\CMS\Plugin\PluginHelper::isEnabled('content', 'responsive')) {
     JLoader::register('Ttc\Freebies\Responsive\Helper', JPATH_ROOT . '/plugins/content/responsive/helper.php');
     $image = (new \Ttc\Freebies\Responsive\Helper)->transformImage($image, [200, 320, 480, 768, 992, 1200, 1600, 1920]);
